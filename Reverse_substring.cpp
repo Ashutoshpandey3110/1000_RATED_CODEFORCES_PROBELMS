350a29a9be8e14a5e221828f63ed02b3164c1d4a
Ashutosh Pandey <158585140+Ashutoshpandey3110@users.noreply.github.com>
2025-08-13 00:24:38 +0530

    1st question

---
+++ Reverse_substring.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    bool flag = false;
+
+    for(int i =0 ; i < n ; i++){
+        if(s[i] < s[i+1]){
+            cout << "Yes" << endl;
+
+            cout << i+1 << " " << i+2 << endl;
+
+            flag = true;
+        }
+    }
+
+    if(!flag){
+        cout << "No" << endl;
+    }
+    return 0;
+}
